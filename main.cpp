bbeb08756c15b2c32a4fc75c3f7e3bcedd87a23a
Agah Özdemir <72024578+aagahoz@users.noreply.github.com>
2021-04-27 08:58:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,29 @@
+#include <string>
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 class eczane
 {
 		int eczaneID;
-		string eczaneIsim;
-		string eczaneAdres;
-	public:
+		char eczaneIsim[10];
+		char eczaneAdres[100];
+	
+	public:	
 		eczane()
 		{
 			this->eczaneID = 0;
-			this->eczaneIsim = "Null";
-			this->eczaneAdres = "Null";
-			cout << "Obje Default Olusturuldu" << endl;
+			strcpy(this->eczaneIsim, "NULL");
+			strcpy(this->eczaneAdres, "NULL");
+			//cout << "Obje Default Olusturuldu" << endl;
 		}
 		
-		eczane(int eczaneID, string eczaneIsim, string eczaneAdres)
+		eczane(int eczaneID, char eczaneIsim[10], char eczaneAdres[100])
 		{
 			this->eczaneID = eczaneID;
-			this->eczaneIsim = eczaneIsim;
-			this->eczaneAdres = eczaneAdres;
-			cout << "Obje Argumanla Olusturuldu" << endl;
+			strcpy(this->eczaneIsim, eczaneIsim);
+			strcpy(this->eczaneAdres, eczaneAdres);
+			//cout << "Obje Argumanla Olusturuldu" << endl;
 		}
 		eczane(const eczane &ecz)
 		{
@@ -31,121 +34,128 @@ class eczane
 			cout << "Obje Assigment Operator ile Olusturuldu" << endl;
 			return *this;
 		}
+		
 		void showInfos()
 		{
-			cout << "ID > " << this->eczaneID << endl << "Isim > " << this->eczaneIsim << endl << "Adres > " << this->eczaneAdres << endl << endl;
+			cout << "ID > " << this->eczaneID  << " Isim > " << this->eczaneIsim << " Adres > " << this->eczaneAdres << endl;
 		}
 		
 		void setEczaneID(int eczaneID)
 		{
 			this->eczaneID = eczaneID;
 		}
-		void setEczaneIsim(string eczaneIsim)
+		void setEczaneIsim(char eczaneIsim[10])
 		{
-			this->eczaneIsim = eczaneIsim;
+			strcpy(this->eczaneIsim, eczaneIsim);
 		}
-		void setEczaneAdres(string eczaneAdres)
+		void setEczaneAdres(char eczaneAdres[100])
 		{
-			this->eczaneAdres = eczaneAdres;
+			strcpy(this->eczaneAdres, eczaneAdres);
 		}
 		
 		int getEczaneID()
 		{
 			return this->eczaneID;
 		}
-		string getEczaneIsim()
+		char *getEczaneIsim()
 		{
 			return this->eczaneIsim;
 		}
-		string getEczaneAdres()
+		char *getEczaneAdres()
 		{
 			return this->eczaneAdres;
 		}
+		
+		void input_Eczane();
+		void eczaneOkuDosyadan();
 };
-////////////////////////////////////////////////////////////////////////////
 
-
-class ilac
+/*
+void input_Eczane()
 {
-		int ilacID;
-		string ilacIsim;
-		int ilacMiktar;
-		double ilacFiyat;
-	public:
-		ilac()
-		{
-			this->ilacID = 0;
-			this->ilacIsim = "Null";
-			this->ilacMiktar = 0;
-			this->ilacFiyat = 0;
-			cout << "Obje Default Olusturuldu" << endl;
-		}
-		
-		ilac(int ilacID, string ilacIsim, int ilacMiktar, double ilacFiyat)
-		{
-			this->ilacID = ilacID;
-			this->ilacIsim = ilacIsim;
-			this->ilacMiktar = ilacMiktar;
-			this->ilacFiyat = ilacFiyat;
-			cout << "Obje Argumanla Olusturuldu" << endl;
-		}
-		ilac(const ilac &ilc)
-		{
-			cout << "Obje Copy Constructor ile Olusturuldu" << endl;
-		}
-		ilac& operator = (const ilac &ilc)
-		{
-			cout << "Obje Assigment Operator ile Olusturuldu" << endl;
-			return *this;
-		}
-		void showInfos()
-		{
-			cout << "ID > " << this->ilacID << endl << "Name > " << this->ilacIsim << endl << "Miktar > " << this->ilacMiktar <<  endl << "Fiyat > " << this->ilacFiyat << endl << endl;
-		}
-		
-		void setIlacID(int ilacID)
-		{
-			this->ilacID = ilacID;
-		}
-		void setIlacIsim(string ilacIsim)
-		{
-			this->ilacIsim = ilacIsim;
-		}
-		void setIlacMiktar(int ilacMiktar)
-		{
-			this->ilacMiktar = ilacMiktar;
-		}
-		void setIlacFiyat(double ilacFiyat)
-		{
-			this->ilacFiyat = ilacFiyat;
-		}
-		
-		int getIlacID()
-		{
-			return this->ilacID;
-		}
-		string getIlacIsim()
-		{
-			return this->ilacIsim;
-		}
-		int getIlacMiktar()
-		{
-			return this->ilacMiktar;
-		}
-		double getIlacFiyat()
-		{
-			return this->ilacFiyat;
-		}
-};
+	int ID1 = 1, ID2 = 2, ID3 = 3, ID4 = 4;
+	char *name1 = "Cicek"; char *name2 = "Arapcesme"; char *name3 = "Baris"; char *name4 = "Sifa";
+	char *adres1 = "MustafapasaMah."; char *adres2 = "ArapcesmeMah."; char *adres3 = "MustafapasaMah."; char *adres4 = "SifaMah";
 
-////////////////////////////////////////////////////////////////////////////
+	eczane e1, e2, e3, e4;
+	e1.setEczaneID(ID1);			e2.setEczaneID(ID2); 			e3.setEczaneID(ID3); 			e4.setEczaneID(ID4);
+	e1.setEczaneIsim(name1);		e2.setEczaneIsim(name2);		e3.setEczaneIsim(name3);		e4.setEczaneIsim(name4);
+	e1.setEczaneAdres(adres1);		e2.setEczaneAdres(adres2);		e3.setEczaneAdres(adres3);		e4.setEczaneAdres(adres4);
 
+	FILE *f = fopen("eczane_listesi.dat", "wb");
+	fwrite((char*)&e1, sizeof(e1), 1, f);
+	fwrite((char*)&e2, sizeof(e2), 1, f);
+	fwrite((char*)&e3, sizeof(e3), 1, f);
+	fwrite((char*)&e4, sizeof(e4), 1, f);
+	fclose(f);
+}
+
+void eczaneOkuDosyadan()
+{
+	eczane e1,e2,e3,e4;
+	
+	dosyaIsim = "eczane_listesi.dat";
+	FILE *f = fopen(dosyaIsim, "rb");
+	
+	fread((char*)&e1, sizeof(e1), 1, f);
+	fread((char*)&e2, sizeof(e2), 1, f);
+	fread((char*)&e3, sizeof(e3), 1, f);
+	fread((char*)&e4, sizeof(e4), 1, f);
+
+	e1.showInfos();
+	e2.showInfos();
+	e3.showInfos();
+	e4.showInfos();
+	fclose(f);
+}
+*/
+void eczane::input_Eczane()
+{
+	int ID1 = 1, ID2 = 2, ID3 = 3, ID4 = 4;
+	char *name1 = "Cicek"; char *name2 = "Arapcesme"; char *name3 = "Baris"; char *name4 = "Sifa";
+	char *adres1 = "MustafapasaMah."; char *adres2 = "ArapcesmeMah."; char *adres3 = "MustafapasaMah."; char *adres4 = "SifaMah";
+
+	eczane e1, e2, e3, e4;
+	e1.setEczaneID(ID1);			e2.setEczaneID(ID2); 			e3.setEczaneID(ID3); 			e4.setEczaneID(ID4);
+	e1.setEczaneIsim(name1);		e2.setEczaneIsim(name2);		e3.setEczaneIsim(name3);		e4.setEczaneIsim(name4);
+	e1.setEczaneAdres(adres1);		e2.setEczaneAdres(adres2);		e3.setEczaneAdres(adres3);		e4.setEczaneAdres(adres4);
+
+	fstream f;
+	f.open("eczane.dat",ios::app|ios::binary);
+	f.write((char*)&e1, sizeof(e1));
+	f.write((char*)&e2, sizeof(e2));
+	f.write((char*)&e3, sizeof(e3));
+	f.write((char*)&e4, sizeof(e4));
+	f.close();
+}
+
+void eczane::eczaneOkuDosyadan()
+{
+	eczane e1,e2,e3,e4;
+
+    fstream f;
+	f.open("eczane.dat", ios::in|ios::binary);	
+	f.read( (char *) &e1, sizeof(e1));
+	f.read( (char *) &e2, sizeof(e2));
+	f.read( (char *) &e3, sizeof(e3));
+	f.read( (char *) &e4, sizeof(e4));
+
+
+	e1.showInfos();
+	e2.showInfos();
+	e3.showInfos();
+	e4.showInfos();
+	f.close();
+}
 
 int main()
 {
+	eczane e1;
+//	e1.input_Eczane();
+
 	int altIslem;
 	int isAdmin;
-	int password = 1234, tempPassword;
+	int password = 1234, tempPassword;	
 	cout << "Eczane otomasyonu baslatildi!" << endl << endl;
 	while(1 == 1)
 	{
@@ -170,6 +180,7 @@ int main()
 					{
 						case 1:
 							cout << "Eczane listesi olustur islemi secildi" << endl;
+							e1.eczaneOkuDosyadan();
 							break;
 						
 						case 2:
@@ -201,19 +212,20 @@ int main()
 					switch(altIslem)
 					{
 						case 1:
-							cout << "Eczane listesi olustur islemi secildi" << endl;
+							cout << "Ilac ara islemi secildi" << endl;
 							break;
 						
 						case 2:
-							cout << "Eczane ekleme islemi secildi" << endl;
+							cout << "Eczane listele islemi secildi" << endl;
+							
 							break;
 							
 						case 3:
-							cout << "Ilac ekleme islemi secildi" << endl;
+							cout << "Herhangi bir eczaneyi arama islemi secildi" << endl;
 							break;
 							
 						case 4:
-							cout << "Ilac duzenleme islemi secildi" << endl;
+							cout << "Herhangi bir ilaci arama islemi secildi" << endl;
 							break;					
 					}
 				break;
